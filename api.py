82a3f3b548aa0b13ae4c5d83619f3d1a0eaa3a4a
koala <jorbjorbjorb69@proton.me>
2022-08-14 21:26:43 -0400

    temp set host to 0.0.0.0

--- api.py
+++ api.py
@@ -564,4 +564,4 @@ api.add_resource(Schedules, '/schedules')
 
 if __name__ == "__main__":
     create_tables()
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True, host="0.0.0.0")
